227793230f738760b44d9b900b00de0511c57470
tulvar-cyber <tulvar@gmail.com>
2026-05-10 09:30:35 -0400

    PSS Task 6 commit correct -1  6-check_files.sh

--- linux_security/0x01_permissions_sguid_sgid/6-check_files.sh
+++ linux_security/0x01_permissions_sguid_sgid/6-check_files.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find "$1" -type f -mtime 0 \( -perm -2000 -o -perm -4000 \) -exec ls -ldb {} \; 2>/dev/null 
+find "$1" -type f -mtime -1 \( -perm -2000 -o -perm -4000 \) -exec ls -ldb {} \; 2>/dev/null 
