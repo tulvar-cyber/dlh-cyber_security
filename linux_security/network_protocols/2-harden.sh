9a1c112c014ac1dda662d77b427d3ac02c8374ff
tulvar-cyber <tulvar@gmail.com>
2026-05-25 09:08:16 -0400

    NP commit 5 the check expect to solve with -exec and not with xargs

--- linux_security/network_protocols/2-harden.sh
+++ linux_security/network_protocols/2-harden.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-find / -xdev -type d -perm -0002 2>/dev/null | tee /dev/tty | xargs chmod o-w
+#!bin/bash
+find / -xdev -type d -perm -0002 -exec chmod o-w {} \; 2>/dev/null | tee /dev/tty 
