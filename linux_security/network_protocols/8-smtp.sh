9ad3710030a4b68e5cf8813cc6781d5b22d5b763
tulvar-cyber <tulvar@gmail.com>
2026-05-26 09:21:12 -0400

    NP Task 8 commit 2 modify the grep to search only at the begining ^

--- linux_security/network_protocols/8-smtp.sh
+++ linux_security/network_protocols/8-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E 'smtpd_tls_security_level\s*=\s*(may|encrypt)' /etc/postfix/main.cf 2>/dev/null || echo "STARTTLS not configured"
+grep -E '^smtpd_tls_security_level\s*=\s*(may|encrypt)' /etc/postfix/main.cf 2>/dev/null || echo "STARTTLS not configured"
