f306fa8c381b58207990e3c2876c29d8a99b4a0b
tulvar-cyber <tulvar@gmail.com>
2026-07-26 13:15:03 -0400

    OWASP commit3 correct

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 ##  Create a Bash script that decode XOR WebSphere
 
 ## Argument
@@ -8,14 +7,13 @@ HASH="$1"
 ## Delete the prefix {xor}
 BASE64="${HASH#\{xor\}}"
 
-echo "$BASE64" | base64 -d 2>/dev/null | while IFS= read -r -n1 char 
+echo "$BASE64" | base64 -d 2>/dev/null | while IFS= read -r -n1 char
 do
-    # Get ASCII 
+    # Get ASCII
     ascii=$(printf "%d" "'$char")
-    # Apply XOR '_' 
-    xor=$((ascii ^ 95))  
+    # Apply XOR '_'
+    xor=$((ascii ^ 95))
     # Convert result and print
     printf "\\$(printf '%03o' $xor)"
 done
-
 echo
